462b6e7dfe12aed8efe74dd6d3c548961efc8706
Karantir <karantir@mail.ru>
2019-03-18 23:11:19 +0300

    task 2

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import readlineSync from 'readline-sync';
+
+export const welcome = () => console.log('Welcome to the Brain Games!');
+export const acquaintance = () => {
+  const actual = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${actual}!`);
+  return actual;
+};
+
+export const gameProcess = () => {
+  welcome();
+  const name = acquaintance();
+};
